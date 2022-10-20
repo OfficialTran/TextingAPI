265f2ccc7eacf54b4dee7c2979cf2e8f2b4d5ac6
Israel <ifq130030@gmail.com>
2022-10-20 01:52:08 -0500

    Add project files.

---
+++ TextingAPI/Controllers/RecieveSMSController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextingAPI.Models;
+using Twilio;
+using Twilio.AspNet.Core;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML;
+using Twilio.Types;
+
+namespace TextingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecieveSMSController : TwilioController
+    {
+
+        [HttpPost("SendReply")]
+        public TwiMLResult SendReply([FromForm] TwilioSMS request)
+        {
+            var response = new MessagingResponse();
+            response.Message("This is me replying from the API, SUP YOUTUBE!");
+
+            return TwiML(response);
+
+        }
+    }
+}
---
+++ TextingAPI/Controllers/SendSMSController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TextingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SendSMSController : ControllerBase
+    {
+        string accountSid = "";
+        string authToken = "";
+
+        [HttpPost("SendText")]
+        public ActionResult SendText(string phoneNumber)
+        {
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Hi To Youtube from my .Net 6 API Using Twilio",
+                from: new Twilio.Types.PhoneNumber("+18315259256"),
+                to: new Twilio.Types.PhoneNumber("+1" + phoneNumber)
+            );
+
+            Console.WriteLine(message.Sid);
+
+            return StatusCode(200, new { message = message.Sid });
+
+        }
+    }
+}
